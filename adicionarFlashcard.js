863fad1e54ef2a161ea6e2c53f23326c376a9350
CauaSilva44 <cauadasilvadias54@gmail.com>
2026-04-29 10:39:34 -0300

    Parte de Adicionar Feito

--- adicionarFlashcard.js
+++ adicionarFlashcard.js
@@ -1,4 +1,29 @@
-function adicionarFlashcard (novoFlashcard, flashcard) {}
+function adicionarFlashcard (novoFlashcard, flashcards, baralhos) {
+
+    let baralhoExiste = false
+    for(let i = 0; i < baralhos.length; i++) {
+        
+        if(baralhos[i].id === novoFlashcard.idBaralho) {
+            baralhoExiste = true
+            break
+        }                
+    }
+        if(!baralhoExiste) {
+            console.log("Baralho não existe.")
+            return false
+        }
+
+        if(flashcards.length > 0) {
+            let ultimoFlashcard = flashcards[flashcards.length - 1]
+            novoFlashcard.id = ultimoFlashcard.id + 1
+        } else {
+            novoFlashcard.id = 1
+        }
+
+        flashcards.push(novoFlashcard)
+        return true
+    }
+
     
 
 export default adicionarFlashcard
